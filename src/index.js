cabc9a069f11272445c8fbd65a6af745b0fccc0e
petrikovandrey <aapetrikov@gmail.com>
2021-06-22 18:24:31 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
-export class Team {
-    constructor() {
-        this.members = new Set();
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      return new Error('Персонаж уже добавлен');
     }
-    add(character) {
-        if(this.members.has(character)){
-            return "Персонаж уже добавлен";
-        }
-        this.members.add(character);
-    }
-    addAll(...array) {
-        array.forEach(element => {
-            this.members.add(element);
-        });
-    }
-    toArray(mySet) {
-        return Array.from(mySet);
-    }
+    this.members.add(character);
+    return this;
+  }
+
+  addAll(...array) {
+    array.forEach((element) => {
+      this.members.add(element);
+    });
+  }
+
+  toArray(mySet) {
+    return Array.from(mySet);
+  }
 }
 
-
-export class Character {
-    constructor(name) {
-        this.name = name;
-        this.health = 100;
-        this.level = 1;
-    }
-    // levelUp() {
-    //     console.log(this);
-    //     if (this.health === 0) {
-    //         return "нельзя повысить левел умершего";
-    //     }
-    //     this.level += 1;
-    //     this.attack += this.attack * 0.2;
-    //     this.defence += this.defence * 0.2;
-    //     this.health = 100;
-    //     console.log(this);
-    // }
-}
+export { Team };
--- src/test.js
+++ src/test.js
@@ -1,44 +1,43 @@
-import * as index from "../src/index.js";
+import * as index from './index';
 
-test("add Team", () => {
-    const character = new index.Character("Gena");
-    const team = new index.Team();
-    team.add(character);
-    const result = team.members;
-    const expected = new Set();
-    expected.add(character);
-    expect(result).toEqual(expected);
-})
-test("duble add", () => {
-    const character = new index.Character("Gena");
-    const team = new index.Team();
-    const expected = "Персонаж уже добавлен";
-    team.add(character);
-    expect(team.add(character)).toBe(expected);
-})
-test("addAll Team", () => {
-    const gena = new index.Character("Gena");
-    const frodo = new index.Character("Frodo");
+test('add Team', () => {
+  const character = new index.Character('Gena');
+  const team = new index.Team();
+  team.add(character);
+  const result = team.members;
+  const expected = new Set();
+  expected.add(character);
+  expect(result).toEqual(expected);
+});
+test('duble add', () => {
+  const character = new index.Character('Gena');
+  const team = new index.Team();
+  const expected = 'Персонаж уже добавлен';
+  team.add(character);
+  expect(team.add(character)).toBe(expected);
+});
+test('addAll Team', () => {
+  const gena = new index.Character('Gena');
+  const frodo = new index.Character('Frodo');
 
-    const team = new index.Team();
-    team.addAll(gena, frodo);
-    const result = team.members;
+  const team = new index.Team();
+  team.addAll(gena, frodo);
+  const result = team.members;
 
-    const expected = new Set();
-    expected.add(gena);
-    expected.add(frodo);
-    expect(result).toEqual(expected);
-})
+  const expected = new Set();
+  expected.add(gena);
+  expected.add(frodo);
+  expect(result).toEqual(expected);
+});
 
-test("to Array", () => {
-    let set = new Set;
-    const result = new index.Team().toArray(set);
-    const expected = [];
-    expect(result).toEqual(expected);
-})
-test("new Character", () => {
-    const result = new index.Character("Pit");
-    const expected =
-        {"health": 100, "level": 1, "name": "Pit"};
-    expect(result).toEqual(expected);
-})
+test('to Array', () => {
+  const set = new Set();
+  const result = new index.Team().toArray(set);
+  const expected = [];
+  expect(result).toEqual(expected);
+});
+// test('new Character', () => {
+//   const result = new index.Character('Pit');
+//   const expected = { health: 100, level: 1, name: 'Pit' };
+//   expect(result).toEqual(expected);
+// });
